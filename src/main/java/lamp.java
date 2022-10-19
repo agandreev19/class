5df585143012d1c22066807431873a66cf6914b6
agandreev19 <agandreev19@codingburgas.bg>
2022-10-19 13:21:53 +0300

    Add lamp

---
+++ src/main/java/lamp.java
@@ -0,0 +1,12 @@
+public class lamp {
+    boolean isOn;
+    void turnOn(){
+        isOn = true;
+        System.out.println("Light on?" + isOn);
+    }
+
+    void turnOff(){
+        isOn = false;
+        System.out.println("Light on?" + isOn);
+    }
+}
